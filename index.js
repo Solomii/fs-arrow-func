1b8f27af7c00c342fc87509c384936fe2369e260
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-01-19 15:05:21 +0200

    Add ways to convey context

---
+++ index.js
@@ -0,0 +1,70 @@
+// "use strict";
+
+// !!!!arrow 
+
+const site = {
+    title: "my site",
+    headers: ["Header1", "About", "Contact"],
+    showHeader: function () {
+        this.headers.forEach((header) => {
+            console.log(this);
+            // console.log(header)
+            console.log(this.title, "|", header);
+        });
+    },
+};
+
+site.showHeader();
+
+// !!!!"костиль" this in variable
+
+// const site = {
+//     title: "my site",
+//     headers: ["Header1", "About", "Contact"],
+//     showHeader: function () {
+//       const self = this;
+//         this.headers.forEach(function(header) {
+//             console.log(self);
+//             // console.log(header)
+//             console.log(self.title, "|", header);
+//         });
+//     },
+// };
+
+// !!!!!! поширений метод передачі this  через function declaration, thisArgs in methods array;
+
+// const site = {
+//   title: "my site",
+//   headers: ["Header1", "About", "Contact"],
+//   showHeader: function () {
+//       // this.headers.forEach(function(header) {
+//       //     console.log(this);
+//       //     // console.log(header)
+//       //     console.log(this.title, "|", header);
+//       // },this);
+//       this.headers.forEach(logHeader,this);
+//   },
+// };
+
+// function logHeader(header) {
+//   console.log(this)
+//   console.log(this.title, "|", header);
+// }
+
+
+
+// bind and use strict
+
+// "use strict";
+// const site = {
+//     title: "my site",
+//     headers: ["Header1", "About", "Contact", "Second title"],
+//     showHeader: function () {
+//         this.headers.forEach(logHeader.bind(this));
+//     },
+// };
+
+// function logHeader(header) {
+//     console.log(this.title, "|", header);
+// }
+// site.showHeader();
