946eb95ebdf0cbf952cc40bd2aab35eab7dd9e68
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-01-20 15:35:05 +0200

    Used throw error

--- js/recursion.js
+++ js/recursion.js
@@ -8,7 +8,7 @@
 //     recursion(num - 1);
 // }
 
-// recursion(10);
+// recursion(10000);
 
 // 2**5 -> 2 *(2**4)
 // 2**4 -> 2 *(2**3)
@@ -18,6 +18,13 @@
 // 2**0 -> 1
 
 // const power = (base, exp) => {
+//   if(typeof base !== number) {
+//     throw new TypeError("type be number")
+//   }
+
+//   if(exp < 0  || exp > 10000) {
+//     throw new RangeError("exp must be positive and have limits 10000")
+//   }
 //     if (base === 0) {
 //         return 1;
 //     }
@@ -33,18 +40,41 @@
 //1! = 1
 //  n! = n * (n-1)
 
-
+// debugger
 const factorial = (num) => {
-  if (num < 0) {
-    return null;
-  }
+    if (typeof num !== number) {
+        throw new TypeError("num must be number");
+    }
 
-  if (num <= 1) {
-    return 1;
-  }
+    if (num < 0 || num > 15) {
+        throw new RangeError("num must be positive and have limits 10000");
+    }
 
-  return num * factorial(num-1);
-  
-}
+    if (num <= 1) {
+        return 1;
+    }
+
+    return num * factorial(num - 1);
+};
 console.log(factorial(5));
 console.log(factorial(-5));
+console.log(factorial(0));
+console.log(factorial(1));
+
+// function recursion(num) {
+//     if (typeof num !== number) {
+//         throw new TypeError("num must be number");
+//     }
+//     if (num > 10000 || num < 0) {
+//         throw new RangeError("num must be 0...10000");
+//     }
+//     if (num === 0) {
+//         return;
+//     }
+//     console.log(num);
+//     recursion(num - 1);
+// }
+
+// console.log(recursion(-4));
+
+// console.log("infirmation");
